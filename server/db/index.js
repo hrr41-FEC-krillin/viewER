78880d42d61e8bf9d40f5a73c8bf4f8e71fa35fb
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-10-11 16:56:29 -0700

    Added api call to respond with sample data

---
+++ server/db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fec', { useNewUrlParser: true });
+
+const MovieSchema = new mongoose.Schema({
+  movieId: {type: Number, unique: true},
+  movieName: String,
+  casts: Array
+});
+
+const Movie = mongoose.model('Movie', MovieSchema);
+
+var getCasts = (movieId, cb) => {
+  Movie.find({ movieId: movieId })
+    .exec((err, docs) => {
+      err ? console.log(err) : cb(docs[0].casts);
+    });
+}
+
+module.exports = { getCasts };
