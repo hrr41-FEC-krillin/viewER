e1172e658e7fd98666343e3fa9f28c543f139200
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-10-13 14:41:12 -0700

    installed jquery and ajax get request

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,47 +1,32 @@
 import React from 'react';
+import $ from 'jquery';
 import CastPhotos from './castPhotos.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      casts: [
-        {
-          "castId": "1",
-          "actor": "Analiese Elsworth",
-          "character": "Jemmy Lyard",
-          "imageUrl": "https://castphotos.s3-us-west-1.amazonaws.com/1.png",
-          "role": "S",
-          "__v": 0
-        },
-        {
-          "castId": "2",
-          "actor": "Minny McCallum",
-          "character": "Dickie Jone",
-          "imageUrl": "https://castphotos.s3-us-west-1.amazonaws.com/2.png",
-          "role": "XS",
-          "__v": 0
-        }]
+      casts: []
     };
-    // this.fetch = this.fetch.bind(this);
+    this.fetch = this.fetch.bind(this);
   }
 
-  // componentDidMount(){
-  //   fetch();
-  // }
+  componentDidMount(){
+    this.fetch();
+  }
 
-  // fetch() {
-  //   $.ajax({
-  //     url: '/api/movie:id',
-  //     method: 'GET',
-  //     data: {id: 1},
-  //     success: (res) => {
-  //       console.log('clent got data:', res);
-  //       this.setState({casts: res});
-  //     },
-  //     error: (err) => console.log('client ajax fail:', err)
-  //   })
-  // }
+  fetch() {
+    $.ajax({
+      url: '/api/movie:id',
+      method: 'GET',
+      data: {id: 1},
+      success: (res) => {
+        console.log('clent got data:', res);
+        this.setState({casts: res});
+      },
+      error: (err) => console.log('client ajax fail:', err)
+    })
+  }
 
   render() {
     return (
