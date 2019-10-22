6ba0365a322f2b5d0ea391c60ac597fea92957f3
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-10-22 16:52:44 -0700

    refactored the seed.js to make it dry

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
 mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const MovieSchema = new mongoose.Schema({
-  movieId: {type: Number, unique: true},
+  movieId: { type: Number, unique: true },
   movieName: String,
-  casts: Array
+  casts: Array,
 });
 
 const MovieModel = mongoose.model('Movie', MovieSchema);
 
-var getCasts = (movieId) => {
-  return MovieModel.find({ movieId: movieId }).exec();
-}
+const getCasts = (movieId) => (
+  MovieModel.find({ movieId }).exec()
+);
 
-module.exports = { getCasts };
+
+module.exports = { getCasts, MovieModel };
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,37 +1,39 @@
 /** Create mock data */
-var movieTitles = ['Squirrel Man', 'Roht: Korangar', 'Revengers: Short Battle', 'Doctor Familiar', 'Guardians of Wu Dao Kou'];
-var actorFirst = ['Jenna', 'Steve', 'John', 'Rainn', 'Angela', 'Ed', 'Mindy', 'BJ', 'Brian', 'Ellie', 'Phyllis', 'Leslie', 'Oscar', 'Kate', 'Creed', 'Paul', 'Craig', 'Melora', 'Rashida'];
-var actorLast = ['Fischer', 'Carell', 'Krasinski', 'Wilson', 'Kinsey', 'Helms', 'Kaling', 'Novak', 'Baumgartner', 'Kemper', 'Smith', 'Baker', 'Nunez', 'Flannery', 'Bratton', 'Lieberstein', 'Robinson', 'Hardin', 'Jones'];
-var characterFirst = ['Pam', 'Michael', 'Jim', 'Dwight', 'Angela', 'Andy', 'Kelly', 'Ryan', 'Kevin', 'Erin', 'Phyllis', 'Stanley', 'Oscar', 'Meredith', 'Creed', 'Toby', 'Darryl', 'Jan', 'Karen'];
-var characterLast = ['Beesly', 'Scott', 'Halpert', 'Schrute', 'Martin', 'Bernard', 'Kapoor', 'Howard', 'Malone', 'Hannon', 'Vance', 'Hudson', 'Martinez', 'Palmer', 'Bratton', 'Flenderson', 'Philbin', 'Levison', 'Filippelli'];
+const movieTitles = ['Squirrel Man', 'Roht: Korangar', 'Revengers: Short Battle', 'Doctor Familiar', 'Guardians of Wu Dao Kou'];
+const actorFirst = ['Jenna', 'Steve', 'John', 'Rainn', 'Angela', 'Ed', 'Mindy', 'BJ', 'Brian', 'Ellie', 'Phyllis', 'Leslie', 'Oscar', 'Kate', 'Creed', 'Paul', 'Craig', 'Melora', 'Rashida'];
+const actorLast = ['Fischer', 'Carell', 'Krasinski', 'Wilson', 'Kinsey', 'Helms', 'Kaling', 'Novak', 'Baumgartner', 'Kemper', 'Smith', 'Baker', 'Nunez', 'Flannery', 'Bratton', 'Lieberstein', 'Robinson', 'Hardin', 'Jones'];
+const characterFirst = ['Pam', 'Michael', 'Jim', 'Dwight', 'Angela', 'Andy', 'Kelly', 'Ryan', 'Kevin', 'Erin', 'Phyllis', 'Stanley', 'Oscar', 'Meredith', 'Creed', 'Toby', 'Darryl', 'Jan', 'Karen'];
+const characterLast = ['Beesly', 'Scott', 'Halpert', 'Schrute', 'Martin', 'Bernard', 'Kapoor', 'Howard', 'Malone', 'Hannon', 'Vance', 'Hudson', 'Martinez', 'Palmer', 'Bratton', 'Flenderson', 'Philbin', 'Levison', 'Filippelli'];
 
-const getRandomInt = function(max, min) {
+function getRandomInt(max, min) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var castId = 0;
-const Cast = function() {
-  this.castId = castId++;
-  this.actor = actorFirst[getRandomInt(actorFirst.length - 1, 0)] + ' ' + actorLast[getRandomInt(actorLast.length - 1, 0)];
-  this.character = characterFirst[getRandomInt(characterFirst.length - 1, 0)] + ' ' + characterLast[getRandomInt(characterLast.length - 1, 0)];
+let castId = 1;
+function Cast() {
+  this.castId = castId;
+  this.actor = `${actorFirst[getRandomInt(actorFirst.length - 1, 0)]} ${actorLast[getRandomInt(actorLast.length - 1, 0)]}`;
+  this.character = `${characterFirst[getRandomInt(characterFirst.length - 1, 0)]} ${characterLast[getRandomInt(characterLast.length - 1, 0)]}`;
   this.imageUrl = `https://castphotos.s3-us-west-1.amazonaws.com/${getRandomInt(40, 0)}.png`;
   this.role = getRandomInt(3, 1);
+  castId += 1;
 }
 
-var movieId = 0;
-const Movie = function() {
-  this.movieId = movieId++;
+let movieId = 1;
+function Movie() {
+  this.movieId = movieId;
   this.title = movieTitles[movieId - 1];
   this.casts = [];
+  movieId += 1;
 }
 
-var populateMovieData = function() {
-  var movieData = [];
+function populateMovieData() {
+  const movieData = [];
 
-  for (var i = 0; i < movieTitles.length; i++) {
-    var someMovie = new Movie();
-    for (var k = 0; k < getRandomInt(30, 20); k++) {
-      var someCast = new Cast();
+  for (let i = 0; i < movieTitles.length; i += 1) {
+    const someMovie = new Movie();
+    for (let k = 0; k < getRandomInt(30, 20); k += 1) {
+      const someCast = new Cast();
       someMovie.casts.push(someCast);
     }
     movieData.push(someMovie);
@@ -40,23 +42,12 @@ var populateMovieData = function() {
   return movieData;
 }
 
-var movieData = populateMovieData();
+const movieData = populateMovieData();
 
-/** Adding mock data to database */
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Console = console;
+const db = require('./index.js');
 
-const MovieSchema = new mongoose.Schema({
-  movieId: {type: Number, unique: true},
-  title: String,
-  casts: Array
-});
-
-const MovieModel = mongoose.model('Movie', MovieSchema);
-
-MovieModel.insertMany(movieData, (err, docs) => {
-  if (err) console.log(`can't add movie data -->`, err);
-  else console.log('added movie data');
+db.MovieModel.insertMany(movieData, (err, _docs) => {
+  if (err) console.error('can\'t add movie data -->', err);
+  else Console.log('added movie data');
 });
