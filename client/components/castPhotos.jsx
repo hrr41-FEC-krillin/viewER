f3abf42de0c56f2ed87186c2233ec816ab3ce9a0
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-12-21 11:40:21 -0800

    change to stateful component

--- client/components/castPhotos.jsx
+++ client/components/castPhotos.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { CastPhotosDiv, CastItem, CastImg, CastActor, CastCharacter } from '../styled.js';
 
-const CastPhotos = (props) => {
-  let last = 6;
-  if (props.ifShow === true) {
-    last = props.data.length;
+class CastPhotos extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <CastPhotosDiv>
-      { props.data.slice(0, last).map(cast => {
-        return (
-          <CastItem key={cast.castId}>
-            <CastImg src={cast.imageUrl} alt={cast.actor}/>
-            <CastActor>{cast.actor}</CastActor>
-            <CastCharacter>as {cast.character}</CastCharacter>
-          </CastItem>
-        )
-        })
-      }
-    </CastPhotosDiv>
-  )
+
+  render(){
+    let last = 6;
+    if (this.props.ifShow === true) {
+      last = this.props.data.length;
+    }
+    return (
+      <CastPhotosDiv>
+        { this.props.data.slice(0, last).map(cast => {
+          return (
+            <CastItem key={cast.castId}>
+              <CastImg src={cast.imageUrl} alt={cast.actor}/>
+              <CastActor>{cast.actor}</CastActor>
+              <CastCharacter>as {cast.character}</CastCharacter>
+            </CastItem>
+          )
+          })
+        }
+      </CastPhotosDiv>
+    )
+  }
 }
 
 export default CastPhotos;
